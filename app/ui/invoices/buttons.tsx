4cf96af238974e2b31cee7f7c0f77129be199f5e
0rregol <83845391+0rregol@users.noreply.github.com>
2026-01-30 03:57:08 -0300

    Refactor DeleteInvoice component for improved clarity

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -27,7 +27,6 @@ export function UpdateInvoice({ id }: { id: string }) {
 
 export function DeleteInvoice({ id }: { id: string }) {
   const deleteInvoiceWithId = deleteInvoice.bind(null, id);
-
   return (
     <form
       action={async () => {
